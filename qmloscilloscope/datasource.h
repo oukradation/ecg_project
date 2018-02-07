0d1a0229962bce336375aa7f0f6b9afeeb4a61ad
odaned <oda.nedrejord@gmail.com>
2018-02-07 17:35:02 +0100

    Add: simple oscilloscope application

---
+++ qmloscilloscope/datasource.h
@@ -0,0 +1,30 @@
+
+//#ifndef DATASOURCE_H
+#define DATASOURCE_H
+
+#include <QtCore/QObject>
+#include <QtCharts/QAbstractSeries>
+
+class QQuickView;
+
+QT_CHARTS_USE_NAMESPACE
+
+class DataSource : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DataSource(QQuickView *appViewer, QObject *parent = 0);
+
+Q_SIGNALS:
+
+public slots:
+    void generateData(int type, int rowCount, int colCount);
+    void update(QAbstractSeries *series);
+
+private:
+    QQuickView *m_appViewer;
+    QList<QVector<QPointF> > m_data;
+    int m_index;
+};
+
+//#endif DATASOURCE_H
---
+++ qmloscilloscope/qmloscilloscope.pro
@@ -0,0 +1,19 @@
+QT += charts qml quick
+
+HEADERS += \
+    datasource.h
+
+SOURCES += \
+    main.cpp \
+    datasource.cpp
+
+RESOURCES += \
+    resources.qrc \
+    resources.qrc
+
+DISTFILES += \
+    qml/qmloscilloscope/*
+
+
+target.path = $$[QT_INSTALL_EXAMPLES]/charts/qmloscilloscope
+INSTALLS += target
