8d1e071ab1d42a262766e5fa57a6425d5842cdd6
odaned <oda.nedrejord@gmail.com>
2018-04-23 00:20:58 +0200

    Add: comment

--- new_oscilloscope/bpm.cpp
+++ new_oscilloscope/bpm.cpp
@@ -1,6 +1,51 @@
 #include "bpm.h"
+#include <iostream>
 
-Bpm::Bpm()
+Bpm::Bpm() :
+    _bpmBuffer(SAMPLE_FREQ),
+    _bpmBuffer_idx(0),
+    _bpm_idx(0),
+    _prev_max_val(0),
+    _current_max_val(0),
+    _timer(0),
+    _nextPeak(true)
 {
 
+}
+
+Bpm::~Bpm()
+{
+
+}
+
+void Bpm::calculateBpm(float signal) {
+
+     _bpmBuffer[_bpmBuffer_idx] = signal;
+
+     if ( (_bpmBuffer_idx % SAMPLE_FREQ/4) == 0)
+     {
+        _current_max_val = *std::max_element(_bpmBuffer.begin(), _bpmBuffer.end());
+        std::cout << _current_max_val << std::endl;
+
+        if( _current_max_val < _prev_max_val && _nextPeak == true && _prev_max_val > 0.4 ) {
+            _bpm_idx++;
+            _nextPeak = false;
+        }
+
+        if( _current_max_val > _prev_max_val ) {
+            _nextPeak = true;
+        }
+
+        _prev_max_val = _current_max_val;
+        _timer++;
+     }
+
+     if( _timer == (4 * 60) )
+     {
+        std::cout << "Bpm: " << _bpm_idx << std::endl;
+        _bpm_idx = 0;
+        _timer = 0;
+     }
+
+     _bpmBuffer_idx = (_bpmBuffer_idx + 1) % SAMPLE_FREQ;
 }
--- new_oscilloscope/bpm.h
+++ new_oscilloscope/bpm.h
@@ -1,11 +1,31 @@
 #ifndef BPM_H
 #define BPM_H
+#include "filtergui.h"
 
+#include <QObject>
+/*
+* Author : Oda Olsen Nedrejord
+*
+* This file contains a bpm calculator, code has not been testet with
+* ecg-signal yet. It probably needs minor modifications after testing
+*/
 
 class Bpm
+    :public QObject
 {
-public:
-    Bpm();
+    public:
+        Bpm();
+        ~Bpm();
+        void calculateBpm(float signal);
+    private:
+        std::vector<float> _bpmBuffer;
+        std::size_t _bpmBuffer_idx;
+        std::size_t _bpm_idx;
+        float _prev_max_val;
+        float _current_max_val;
+        std::size_t _timer;
+        bool _nextPeak;
+
 };
 
 #endif // BPM_H
